d91ea2e1c32605d743ee21036db08ca7cbc593e3
Pablo <pablosanse13@gmail.com>
2024-04-18 22:13:30 +0200

    Flecha y Visuals

---
+++ custom_resources/card_pile.gd
@@ -0,0 +1,26 @@
+class_name  CardPile
+extends Resource
+
+signal card_pile_size_changed(cards_amount)
+
+@export var cards: Array[Card] = []
+
+func empty() -> bool:
+	return cards.is_empty()
+
+func draw_card() -> Card:
+	var card = cards.pop_front()
+	card_pile_size_changed.emit(card.size())
+	return card
+	
+func add_card(card: Card) -> void:
+	cards.append(card)
+	card_pile_size_changed.emit(cards.size())
+	
+
+func suffle() -> void:
+	cards.shuffle()
+
+func clear() -> void:
+	cards.clear()
+	card_pile_size_changed.emit(cards.size())
---
+++ scenes/card_ui/card_states/card_released_state.gd
@@ -0,0 +1,15 @@
+extends CardState
+
+var played: bool
+
+func enter() -> void:
+	played= false
+	
+	if not card_ui.targets.is_empty():
+		played = true
+		print(card_ui.targets)
+
+func on_input(_event: InputEvent)-> void:
+	if played:
+		return
+	transition_requested.emit(self, CardState.State.BASE)
